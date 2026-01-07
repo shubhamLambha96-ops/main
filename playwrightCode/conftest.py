db6b4d467d015f9bff8ec10789cf746217443013
Shubham Lambha <ShubhamLambha.1996@gmail.com>
2026-01-07 17:07:27 +0530

    FrameWork code

--- playwrightCode/conftest.py
+++ playwrightCode/conftest.py
@@ -5,3 +5,7 @@ import pytest
 def secondCheck():
     print("browser setup second check")
     return "pass"
+
+@pytest.fixture(scope = "session")
+def user_credentials(request):
+    return request.param
